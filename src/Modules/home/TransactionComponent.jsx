3995e6936e0dd52c78d6a5ea48286badd03a7d48
Duncan Kibet <kibet27@gmail.com>
2025-04-15 07:46:44 +0300

    add search feature

--- src/Modules/home/TransactionComponent.jsx
+++ src/Modules/home/TransactionComponent.jsx
@@ -1,17 +1,20 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-margin: 30px 0 10px;
-font-family: Montserrat;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 30px 0 10px;
+  font-family: Montserrat;
 `;
 
-
-const TransactionComponent= (props) => {
-    return <Container>TransactionComponent</Container>;
-      
-    
+const TransactionComponent = (props) => {
+  return (
+    <Container>
+      <div>TransactionComponent</div>
+      <input placeholder="Search" />
+    </Container>
+  );
 };
+
 export default TransactionComponent;
